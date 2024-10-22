10e15b451fb7cb4962914217006b9472f872744d
TylerTHChen <tchen456@wisc.edu>
2024-10-22 17:20:15 -0500

    finished all stages

--- demo1/verilog/decode.v
+++ demo1/verilog/decode.v
@@ -5,40 +5,65 @@
    Description     : This is the module for the overall decode stage of the processor.
 */
 `default_nettype none
-module decode (instruction, nextPC, asource, bsource, sign_extended_11bit, 8bit, aluOP,
-               regsrc, immsrc, memwrt, inva, invb, alujmp, aluopr, branch);
-   input [15:0] instruction;
-   input [15:0] nextPC;
-   output [15:0] asource;
-   output [15:0] bsource;
-   output [15:0] sign_extended_11bit;
-   output [15:0] 8bit;
+module decode ();
+   input clk;
+   input rst;
+   output err;
+   // input - wires
+   input [15:0]PC_in;
+   input [15:0]instruction;
+   input [15:0]wb_data;
+   input [15:0]next_PC_in;
 
-   // Instruction Decoder
+   // output -wires
+   output [15:0] PC_out;
+   output [15:0] next_PC_out;
+   output [15:0]srca;
+   output [15:0]srcb;
+   output [15:0]8bits;
+   output [15:0]11bits;
+   output [1:0]instruct;
 
-   wire [15:0] sign_extended_5bit;
-   wire [15:0] sign_extended_8bit;
-   wire [15:0] zero_extended_5bit;
-   wire [15:0] zero_extended_8bit;
-   wire [15:0] 5bit;
-   wire [15:0] rd2;
-   wire 0ext;
+   // output - decode
+   output regsrc;
+   output immsrc;
+   output memwrt;
+   output inva;
+   output invb;
+   output [2:0]branch;
+   output alujmp;
+   output [3:0]aluopr;
 
-   regFile_bypass rf(.read1Data(), .read2Data(), .err(), .clk(), .rst(), .read1RegSel(instruction[10:8]), .read2RegSel(instruction[7:5]), .writeRegSel(), .writeData(), .writeEn())
-
-   assign sign_extended_5bit = {{11{instruction[4]}}, instruction[4:0]};
-   assign sign_extended_8bit = {{8{instruction[7]}}, instruction[7:0]};
-   assign sign_extended_11bit = {{5{instruction[10]}}, instruction[10:0]};
-   assign zero_extended_5bit = {11'b0, instruction[4:0]};
-   assign zero_extended_8bit = {8'b0, instruction[7:0]};
-
-   mux2_1 5bit_mux(.in0(sign_extended_5bit), .in1(zero_extended_5bit), .sel(0ext), .out(5bit));
-   mux2_1 8bit_mux(.in0(sign_extended_8bit), .in1(zero_extended_8bit), .sel(0ext), .out(8bit));
-
-   mux4_1 bsource_mux(.in0(rd2), .in1(5bit), .in2(8bit), .in3(sign_extended_11bit), .sel(bsource_sel), .out(bsource));
-
-
-
+   wire regdst, 0ext, regwrt, asource, bsource;
+   instruction_decoder instrucDecode(.instruction(instruction[15:11]), .regdst(regdst), .0ext(0ext), 
+                                    .regwrt(regwrt), .bsource(bsource), .branch(branch), .aluop(aluopr), 
+                                    .alujmp(alujmp), .invb(invb), .inva(inva), .memwrt(memwrt), .immsrc(immsrc),
+                                    .asource(asource), .regsrc(regsrc));
    
+   wire [2:0]wrtreg;
+   mux4_1 #(3) muxWriteRegister(.in0(instruction[7:5]), .in1(instruction[10:8]), .in2(instruction[4:2]), 
+                                 .in3(3'b111), .sel(regdst), .out(wrtreg));
+
+   wire[15:0]src1, src2;
+   regFile_bypass registerFile(read1Data(src1), read2Data(src2), err(err), clk(clk), 
+                              rst(rst), read1RegSel(instruction[10:8]), read2RegSel(instruction[7:5]),
+                              writeRegSel(wrtreg), writeData(wb_data), writeEn(regwrt));
+                     
+   wire [15:0] SE_5bits, ZE_5bits, SE_8bits, ZE_8bits 5bits;
+   assign SE_5bits = {{11{instruction[4]}}, instruction[4:0]};
+   assign ZE_5bits = {{11'b0}, instruction[4:0]};
+   assign SE_8bits = {{8{instruction[7]}}, instruction[7:0]};
+   assign ZE_8bits = {{8'b0}, instruction[7:0]};
+   assign 11bits = {{5{instruction[10]}}, instruction[10:0]};
+
+   mux2_1 mux5bit(.in0(SE_5bits), .in1(ZE_5bits), .sel(0ext), .out(5bits));
+   mux2_1 mux5bit(.in0(SE_8bits), .in1(ZE_8bits), .sel(0ext), .out(8bits));
+   wire [15:0]shftSrcA;
+   assign shftSrcA = src1 << 8;
+   mux4_1 bsourceMux(.in0(src2), .in1(5bits), .in2(8bits), .in3(11bits), sel(bsource), .out(srcb));
+   mux4_1 asourceMux(.in0(src1), .in1(shftSrcA), .sel(asource), .out(srca));
+   assign instruct = instruction[1:0];
+   assign PC_out = PC_in;
+   assign next_PC_out = next_PC_in;
 endmodule
 `default_nettype wire
--- demo1/verilog/execute.v
+++ demo1/verilog/execute.v
@@ -5,15 +5,15 @@
    Description     : This is the overall module for the execute stage of the processor.
 */
 `default_nettype none
-module execute (PC_in, 8bits, 10bits, asrc, bsrc, next_PC_in, instruct, wb_in, 
+module execute (PC_in, 8bits, 11bits, asrc, bsrc, next_PC_in, instruct, wb_in, 
 regsrc, immsrc, memwrt, inva, invb, branch, alujmp, aluopr, PC_out, jmpSource, 
 alu_out, srcb_out, next_PC_out, wb_out, setrd, regsrc_out, alujmp_out, brchcnd, memwrt_out);
    
    //////////// INPUTS /////////////
    // wires
    input [15:0]PC_in;
-   input [7:0]8bits;
-   input [8:0]10bits;
+   input [15:0]8bits;
+   input [15:0]11bits;
    input [15:0]asrc;
    input [15:0]bsrc;
    input [15:0]next_PC_in;
@@ -53,7 +53,7 @@ alu_out, srcb_out, next_PC_out, wb_out, setrd, regsrc_out, alujmp_out, brchcnd, 
 
    branchCondition bc1(.branch(branch), .SF(SF), .ZF(ZF), .OF(OF), .CF(CF), .brchcnd(brchcnd), .setrd(setrd));
 
-   mux2_1 mux1(.in0(8bits), .in1(10bits), .sel(immsrc), .out(jmpSource));
+   mux2_1 mux1(.in0(8bits), .in1(11bits), .sel(immsrc), .out(jmpSource));
 
    assign PC_out = PC_in;
    assign srcb_out = bsrc;
